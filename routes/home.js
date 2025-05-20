eed535e25c856afd48fd4ceb7ceaf4f19d49851c
mercy-ndiritu <mewanjikundiritu@gmail.com>
2025-05-20 21:24:04 +0300

    feat: change home route from POST to GET method

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const express = require("express");
 const homeRouter = express.Router();
 
 
-homeRouter.post("/", async (req, res) => {
+homeRouter.get("/", async (req, res) => {
     res.status(200).json({
         message: "Hello from the home route!",
         
